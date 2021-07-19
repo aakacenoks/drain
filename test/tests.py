efba37e4017d36959a51147d3bb4a5feab278f0d
Andris <andrisakacenoks@gmail.com>
2021-07-19 22:43:55 +0300

    Add server tests

--- test/tests.py
+++ test/tests.py
@@ -1,15 +1,21 @@
 import requests
 
+BASE_URL = "http://localhost:5000/api"
 
 def test_cycle_mode():
-    response = requests.get("http://localhost:5000/api/cycle")
+    response = requests.get(f'{BASE_URL}/cycle')
     response_body = response.json()
     assert response.status_code == 200
     assert response_body["message"] == "Cycle mode enabled"
 
-
 def test_connect():
-    response = requests.get("http://localhost:5000/api/connect")
+    response = requests.get(f'{BASE_URL}/connect')
     response_body = response.json()
     assert response.status_code == 200
     assert response_body["message"] == "Cycle mode disabled. All ports connected."
+
+def test_status():
+    response = requests.get(f'{BASE_URL}/status')
+    response_body = response.json()
+    assert response.status_code == 200
+    assert len(response_body) > 0
