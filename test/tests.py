4f2c51331aae805ef62a5f811e836660908ab46b
tdl.andrisa <andris.akacenoks@testdevlab.com>
2021-07-20 22:52:55 +0300

    fix tests

--- test/tests.py
+++ test/tests.py
@@ -3,7 +3,7 @@ import requests
 BASE_URL = "http://localhost:5004/api"
 
 def test_cycle_mode():
-    response = requests.get(f'{BASE_URL}/cycle')
+    response = requests.post(f'{BASE_URL}/cycle')
     response_body = response.json()
     assert response.status_code == 200
     assert response_body["message"] == "Cycle mode enabled"
@@ -15,7 +15,7 @@ def test_connect():
     assert response_body["message"] == "Cycle mode disabled. All ports connected."
 
 def test_status():
-    response = requests.post(f'{BASE_URL}/status')
+    response = requests.get(f'{BASE_URL}/status')
     response_body = response.json()
     assert response.status_code == 200
     assert len(response_body) > 0
