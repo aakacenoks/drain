c8ef584335f93cf01bcecefe69ca217dad8a1866
tdl.andrisa <andris.akacenoks@testdevlab.com>
2021-09-07 16:59:43 +0300

    handle usb hub connection issues

--- src/hub_manager.py
+++ src/hub_manager.py
@@ -8,26 +8,30 @@ def connect_to_hub(serial_number):
     result = stem.discoverAndConnect(1, serial_number=serial_number)
     if result == Result.NO_ERROR:
         return stem
-    log.info("error connecting to USB hub. make sure you are using the correct module object")
+    log.warning("error connecting to USB hub. make sure you are using the correct module object")
 
 def disable_all_ports(serial_number):
     stem = connect_to_hub(serial_number)
-    for port in range(0, 8):
-        stem.usb.setPortDisable(port)
-    stem.disconnect()
+    if stem:
+        for port in range(0, 8):
+            stem.usb.setPortDisable(port)
+        stem.disconnect()
 
 def enable_all_ports(serial_number):
     stem = connect_to_hub(serial_number)
-    for port in range(0, 8):
-        stem.usb.setPortEnable(port)
-    stem.disconnect()
+    if stem:
+        for port in range(0, 8):
+            stem.usb.setPortEnable(port)
+        stem.disconnect()
 
 def disable_port(serial_number, port_number):
     stem = connect_to_hub(serial_number)
-    stem.usb.setPortDisable(port_number)
-    stem.disconnect()
+    if stem:
+        stem.usb.setPortDisable(port_number)
+        stem.disconnect()
 
 def enable_port(serial_number, port_number):
     stem = connect_to_hub(serial_number)
-    stem.usb.setPortEnable(port_number)
-    stem.disconnect()
+    if stem:
+        stem.usb.setPortEnable(port_number)
+        stem.disconnect()
