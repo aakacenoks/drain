101fc063b498853e86cb071442d48c8550abf137
aakacenoks <andrisakacenoks@gmail.com>
2021-12-16 14:45:40 +0200

    print first element

--- src/main.py
+++ src/main.py
@@ -94,7 +94,7 @@ def search(udid):
                         log.info(f"device found. info: {device_info}")
                         return device_info, 200
                     else:
-                        log.info(f'searched device did not disconnect. the last disconnected device was {missing_devices[-1]}')
+                        log.info(f'searched device did not disconnect. the last disconnected device was {missing_devices[0]}')
             return {'message': f'device ({udid}) is connected, but could not be disconnected'}, 404
         else:
             return {'message': f'device ({udid}) is not connected to any of the hubs ({devices.hubs})'}, 404
